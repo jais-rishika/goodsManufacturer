4fcdc5fa823f773f672e0b37ea97c2de6c357a28
rishikajaiswal <jaisrishika@gmail.com>
2025-06-27 22:44:35 +0530

    deployed server added

--- src/routes/route.guards.ts
+++ src/routes/route.guards.ts
@@ -1,21 +1,22 @@
+
 export const checkLogin = () => {
     return !!localStorage.getItem("token")
 }
 
 export const isOwner = () => {
-    
+    return true    
 };
 
 export const isFacilityManager = () =>{
-    
+    return true    
 } 
 
 export const isWorkplaceManager = () =>{
-    
+    return true    
 } 
 
 export const isToolCribManager = () =>{
-    
+    return true    
 } 
 
 export const isWorker = () =>{
